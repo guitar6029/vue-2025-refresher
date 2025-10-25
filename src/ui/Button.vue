c6d3d827b5e49003757ce9eb84325a1998aef342
Josh <jsbusiness305@gmail.com>
2025-10-25 16:32:09 -0400

    added Button Component

---
+++ src/ui/Button.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+
+const emit = defineEmits<({
+    onClick: []
+})>();
+
+const props = withDefaults(
+    defineProps<{
+        text?: string
+    }>(),
+    {
+        text: "Click"
+    }
+)
+
+</script>
+
+<template>
+    <button
+        class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        {{ props.text }}
+    </button>
+</template>
