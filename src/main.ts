6527221309c14de4eb5b7c48452bcec73f5ffdba
Josh <jsbusiness305@gmail.com>
2025-10-25 19:59:45 -0400

    added vue toastify package

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import 'vue3-toastify/dist/index.css' // required styles
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify'
 
 
 const pinia = createPinia();
@@ -10,4 +12,8 @@ const pinia = createPinia();
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
+app.use(Vue3Toastify, {
+    autoClose: 3000,
+    position: "top-right",
+} as ToastContainerOptions);
 app.mount('#app');
