251c4795e454a2be47585328af6643f641d0fcc0
Josh <jsbusiness305@gmail.com>
2025-10-25 18:11:43 -0400

    updated home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <main class="max-w-[600px] flex items-center justify-center mx-auto">
-        <h1>Vue Refresher 2025</h1>
+    <main class="max-w-[600px] min-h-screen flex flex-col items-center justify-center mx-auto">
+        <h1 class="text-5xl">Vue Refresher 2025</h1>
     </main>
 </template>
