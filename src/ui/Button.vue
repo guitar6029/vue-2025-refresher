0ea02e903fb2879fb10afeb58acc2e0d1547c7c3
Josh <jsbusiness305@gmail.com>
2025-10-25 16:50:30 -0400

    updated Button component

--- src/ui/Button.vue
+++ src/ui/Button.vue
@@ -16,8 +16,7 @@ const props = withDefaults(
 </script>
 
 <template>
-    <button
-        class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+    <button class="btn" @click="emit('onClick')">
         {{ props.text }}
     </button>
 </template>
