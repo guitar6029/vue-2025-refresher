7880df369e65b28059f2df26aa0639dbfd29d7f6
Josh <jsbusiness305@gmail.com>
2025-10-25 20:54:14 -0400

    added RadialProgress component

---
+++ src/ui/RadialProgress.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+defineProps<{
+    value: number
+    size?: string
+    color?: string
+}>()
+</script>
+
+<template>
+    <div class="radial-progress" :class="color || 'text-primary'" role="progressbar" :style="{
+        '--value': value,
+        '--size': size || '4rem',
+        '--thickness': '4px'
+    }">
+        {{ Math.round(value) }}%
+    </div>
+</template>
